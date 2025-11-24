bee6c4512478caf010da0210b9e951f7678a1599
Alan <67723585+LLLLLLYOHO@users.noreply.github.com>
2025-11-25 00:02:53 +0800

    ampDache.js
    高德获取Cookies

---
+++ ampDache.js
@@ -0,0 +1,9 @@
+#高德签到————————原始链接
+#^https:\/\/m5(-zb)?\.amap\.com\/ws\/yuece\/(act|openapi)\/(activity\/current\/)?query url script-response-body https://raw.githubusercontent.com/wf021325/qx/master/task/ampDache.js
+
+#高德签到————————改成自己的脚本，防止被删库
+^https:\/\/m5(-zb)?\.amap\.com\/ws\/yuece\/(act|openapi)\/(activity\/current\/)?query url script-response-body https://raw.githubusercontent.com/LLLLLLYOHO/Script/refs/heads/main/ampDache.js
+
+
+#捷停车
+^https:\/\/sytgate\.jslife\.com\.cn\/core-gateway\/order\/carno\/pay\/info url script-request-body https://raw.githubusercontent.com/FoKit/Scripts/main/scripts/jparking_sign.js
