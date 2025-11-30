981a6db85ffdf4a6af64ee69643c874ee7e5dd8f
Alan <67723585+LLLLLLYOHO@users.noreply.github.com>
2025-11-30 17:20:00 +0800

    Update script URL for COTTI coffee app

---
+++ cotti.js
@@ -0,0 +1,5 @@
+# > 库迪咖啡净化<微信小程序> - COTTI
+^https?:\/\/gateway\.cotticoffee\.com\/cotti-capi\/customer\/position\/list$ url script-request-body https://raw.githubusercontent.com/LLLLLLYOHO/Script/refs/heads/main/cotti.js
+^https?:\/\/gateway\.cotticoffee\.com\/cotti-capi\/customer\/position\/adSortList url reject-dict
+
+hostname = gateway.cotticoffee.com
